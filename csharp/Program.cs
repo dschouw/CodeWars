7adb9973b540d024941dd8b764f65323b5f44d52
Denver Schouw <dschouw@gmail.com>
2020-04-25 16:56:32 +0200

    Reverse String X amount of times
    Didnt complete. Algorithm works but not performant enough.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -11,6 +11,10 @@ namespace csharp
             Console.WriteLine(OddEvenStringSort.SortMyString("CodeWars"));
 
             Console.WriteLine(ReplaceWithAlphabetPosition.AlphabetPosition("The sunset sets at twelve o' clock."));
+
+            System.Console.WriteLine(StringXIterationsString.StringFunc("Ohh Man God Damn 2424234",20));
+
+            System.Console.WriteLine(StringXIterationsString.ReverseString("Ohh Man God Damn 2424234",20));
         }
     }
 }
